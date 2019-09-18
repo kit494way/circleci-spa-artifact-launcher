08bce427ce897df77b7d28793cf51a7f63092d39
KITAGAWA Yasutaka <kit494way@gmail.com>
2019-09-19 07:12:27 +0900

    launch server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+extern crate iron;
+extern crate mount;
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
+extern crate staticfile;
 
 use std::env;
 use std::fs;
@@ -8,32 +11,69 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::io::Write;
 use std::path::Path;
+use std::path::PathBuf;
 use std::str::FromStr;
 
+use iron::prelude::*;
+use iron::AfterMiddleware;
+use mount::Mount;
 use serde::Deserialize;
+use staticfile::Static;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let circle_token = env::var("CIRCLE_TOKEN").expect("missing CIRCLE_TOKEN environemt variable");
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() != 5 {
-        writeln!(
-            std::io::stderr(),
-            "Usage: {} VCS USER PROJECT BUILD_NUM",
-            Path::new(&args[0]).file_name().unwrap().to_str().unwrap()
-        )
-        .unwrap();
-        std::process::exit(1);
+    let port = env::var("PORT").unwrap_or("3000".to_string());
+
+    let vcs = nth_arg_or_exit(1);
+    let user = nth_arg_or_exit(2);
+    let project = nth_arg_or_exit(3);
+    let build_num = u32::from_str(nth_arg_or_exit(4).as_str()).unwrap();
+
+    let downloaded_dir = download_artifacts(vcs, user, project, build_num, circle_token).unwrap();
+
+    let mut mount = Mount::new();
+    mount.mount("/", Static::new(downloaded_dir.clone()));
+
+    let mut chain = Chain::new(mount);
+    chain.link_after(NotFoundResponsePath {
+        path: downloaded_dir.join(Path::new("index.html")),
+    });
+
+    println!("Start server localhost:{}", port);
+    Iron::new(chain).http(format!("localhost:{}", port)).unwrap();
+}
+
+fn nth_arg_or_exit(nth: usize) -> String {
+    let arg = std::env::args().nth(nth);
+    if arg.is_none() {
+        print_usage_and_exit();
     }
+    arg.unwrap()
+}
 
-    let vcs = &args[1];
-    let user = &args[2];
-    let project = &args[3];
-    let build_num = u32::from_str(&args[4]).unwrap();
+fn print_usage_and_exit() {
+    let program = std::env::args().nth(0).unwrap();
+    let program_name = Path::new(&program).file_name().and_then(|f| f.to_str()).unwrap();
+    writeln!(
+        std::io::stderr(),
+        "Usage: {} VCS USER PROJECT BUILD_NUM",
+        program_name
+    )
+    .unwrap();
+    std::process::exit(1);
+}
 
+fn download_artifacts(
+    vcs: String,
+    user: String,
+    project: String,
+    build_num: u32,
+    token: String,
+) -> Result<PathBuf, Box<dyn std::error::Error>> {
     let circle_job = CircleCIBuild {
-        vcs: String::from(vcs),
-        user: String::from(user),
-        project: String::from(project),
+        vcs: vcs,
+        user: user,
+        project: project,
         build_num: build_num,
     };
 
@@ -44,10 +84,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         project = circle_job.project,
         build_num = circle_job.build_num
     );
+    let dest_path = Path::new(&dest);
 
-    circle_job.download_artifacts(&circle_token, Path::new(&dest)).unwrap();
+    circle_job.download_artifacts(&token, dest_path)?;
 
-    Ok(())
+    Ok(PathBuf::from(dest_path))
 }
 
 struct CircleCIBuild {
@@ -138,4 +179,19 @@ impl LCP for Vec<String> {
 #[derive(Deserialize, Debug)]
 struct CircleCIArtifact {
     url: String,
+}
+
+struct NotFoundResponsePath {
+    path: PathBuf,
+}
+
+impl AfterMiddleware for NotFoundResponsePath {
+    fn catch(&self, _: &mut Request, err: IronError) -> IronResult<Response> {
+        match err.response.status {
+            Some(iron::status::NotFound) => {
+                Ok(Response::with((iron::status::Ok, self.path.clone())))
+            }
+            _ => Err(err),
+        }
+    }
 }
